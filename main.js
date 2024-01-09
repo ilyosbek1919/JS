d6a9dc820e22266b7e4b45cbd0fd3453a357e4c0
ilyos1919 <qalandarovilyos75@gmail.com>
2024-01-09 12:43:39 +0500

    ilyos

---
+++ main.js
@@ -0,0 +1,45 @@
+console.log("savol javobni boshladik")
+let ism = prompt("Ismingiz nima? ")
+if("ism"){
+    alert("?")
+}else{
+    alert("Raxmat")
+}
+let age = prompt('yoshingiz nechida')
+if (age < 18) {
+    alert("ukam tengi ekansiz ")
+} else {
+    alert("do'st bo'lsak bo'larkan");
+}
+
+let manzil = prompt("Qaerdansiz?", "Qashqadaryo")
+if (manzil) {
+    alert("Zemlyak ekanmiz")
+} else {
+    alert('Ha u joyni bilmas ekanman');
+}
+
+
+
+let tel = prompt("Telefon raqamanigizni kiriting", "880077574")
+if (tel) {
+    alert("Raxmat")
+} else {
+    alert("unda siz bilan qanday bog'lanamiz");
+}
+let info = prompt("Siz o'qiysizmi?-Ishlaysizmi?")
+if (info) {
+    alert("Qerda")
+} else {
+    alert("Qanaqa ishda ishlaysiz sir bo'lmasa?");
+}
+
+let hobbi = prompt("hobbiyingiz?")
+if (hobbi) {
+    alert("juda yaxshi");
+}
+
+let al = alert("vazifa")
+console.log(al);
+let con = confirm("yoqdimi?")
+console.log(con);
